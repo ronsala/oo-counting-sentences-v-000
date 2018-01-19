2dbe1b3ffcbc00067a7c827f91ab62dca89db2d2
Ron Sala <ron.sala@gmail.com>
2018-01-19 03:29:39 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    ary = self.split(/[.?!]/)
+    ary.reject! {|e| e == ""}
+    ary.count
   end
 end
